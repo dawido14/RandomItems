b4e08bc27a6458e434eb631ea6751fa50199b537
Dawido <73956643+dawido14@users.noreply.github.com>
2021-09-06 17:30:16 +0200

    Add files via upload

---
+++ RandomItems/RandomItems/LogSystem.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.API.Features;
+
+namespace RandomItems
+{
+    public static class LogSystem
+    {
+        public static void OnPlayerGetItem(string item, string playerNickname)
+        {
+            if (RandomItems.newRandomItems.Config.LogWhenPlayerGetItem)
+            {
+                Log.Info($"{playerNickname} get {item}!");
+            }
+        }
+    }
+}
